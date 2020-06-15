25702c2ddef833d2d2eeaaaea9accccd91661c87
Greta Freitag <greta.freitag@gmail.com>
2020-06-15 14:49:45 -0700

    added example/changed prediction script to work on example recipe

--- python/prediction.py
+++ python/prediction.py
@@ -3,25 +3,9 @@ from flair.models import SequenceTagger
 
 model = SequenceTagger.load('output/best-model.pt')
 
-fr=open('Pork-Medallions-with-Port-Sauce.txt','r')
+fr=open('../data/example_recipe.txt','r')
 for line in fr:
     sentence=Sentence(line)
     model.predict(sentence)
     print(sentence.to_tagged_string())
-'''
-sentence = Sentence('Add the pasta and stir')
 
-model.predict(sentence)
-print(sentence.to_tagged_string())
-
-sentence = Sentence('Set oven to 350 degrees Fahrenheit and bake for 30 minutes to an hour')
-
-model.predict(sentence)
-print(sentence.to_tagged_string())
-
-sentence = Sentence('3/4 cup chopped red onions')
-
-model.predict(sentence)
-print(sentence.to_tagged_string())
-
-'''
