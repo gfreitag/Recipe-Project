0b9a8a9ae710f8f4a7147026e7d80efdadf617c2
Greta Freitag <greta.freitag@gmail.com>
2020-07-09 14:07:23 -0700

    reformatted script and added pseudo-pseudo-code

--- python/word2vec.py
+++ python/word2vec.py
@@ -1,10 +1,11 @@
 '''
-Refrences this tutorial: https://towardsdatascience.com/implementing-word2vec-in-pytorch-skip-gram-model-e6bae040d2fb
+References this tutorial: https://towardsdatascience.com/implementing-word2vec-in-pytorch-skip-gram-model-e6bae040d2fb
 '''
 
 from ingredientExtractor import *
 import torch
 import random
+
 
 def get_input_layer(word_idx):
     x = torch.zeros(vocabulary_size).float()
@@ -12,23 +13,24 @@ def get_input_layer(word_idx):
     return x
 
 
-if __name__ == "__main__":
-    random.seed()
-    filename = input('Ingredient list: ')
-    recipe = input('Recipe: ')
+def get_index_ingreds():
     indexToWord = {}
     wordToIndex = {}
+    filename = input('Ingredient list: ')
+    for counter, item in enumerate(filename):
+        indexToWord[counter] = item
+        wordToIndex[item]=counter
+    return indexToWord, wordToIndex,
+
+
+def setup(recipe, wordToIndex):
+    random.seed()
 
     modelDir = 'fullModel'
     extractor = Extractor(modelDir, recipe)
 
     ingredDict = extractor.compile_set_shallow()
-    ingredLiist = list(ingredDict.items())
-
-    for counter, item in enumerate(filename):
-        indexToWord[counter] = item
-        wordToIndex[item]=counter
-    volume = len(indexToWord)
+    ingredList = list(ingredDict.items())
 
     id_pairs=[]
     for i in range(0, 20):
@@ -39,3 +41,27 @@ if __name__ == "__main__":
         while center == context:
             context = random.choice(ingredList)
         id_pairs.append(wordToIndex[center], wordToIndex[context])
+
+    return id_pairs
+
+
+def train_loop(epochs, train_rate):
+    for i in range(0, epochs):
+        # For every recipe:
+            id_pairs = setup(recipe, wordToIndex)
+            # convert to one-hot-encoding
+            # run through weights?
+            # back propagate
+            # Use clever pytorch tools
+    # return loss, etc
+
+
+if __name__ == "__main__":
+    recipe = None       # temp
+    indexToWord, wordToIndex = get_index_ingreds()
+    train_loop(100, 0.001)
+
+
+
+
+
